8d06bd88f51b27ca0b30b1234d6428f3b55e72ee
voitd <dimytr.voitenko@gmail.com>
2023-03-06 02:41:45 +0200

    fix: deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
+        <BrowserRouter basename="/AlterEGO-Test">
           <App />
         </BrowserRouter>
       </ThemeProvider>
